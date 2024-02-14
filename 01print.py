11cb0358e42e8b72d46ac7f80abbc30d123827fa
alejoprogramaddor <aristizabal-1996@hotmail.com>
2024-02-14 18:06:37 -0500

    mi primer montaje a git

---
+++ 01print.py
@@ -0,0 +1,51 @@
+"""la primera funcion que vamos a guardar es print 
+y funciona para imprimir en consola esto. """
+
+print("hola soy alejandro y estoy programando")
+
+"""tambien se puede poner como una calculadora , para suma , resta 
+multiplicacion y division """
+
+print(12+5)
+print(18-5)
+print (12 * 120)
+print(12/2)
+
+"""tambien en python hay algo que se llama identacion , esto son los 
+espacios que hay al principio de el texto """
+
+#esto es para un comentario de una sola linea 
+
+"""esto es para un comentario de 
+varias lineas"""
+
+'''tambien esta se puede utilizar para hacer un comentario de varias lineas '''
+
+'''tambien esta es la forma de crear una variable '''
+my_name = 'alejandro'
+
+print(my_name)
+
+'''como podemos observar en print aparece el significado de la variable 
+que es un string que es alejandro '''
+
+#otro ejemplo es este pero con un numero 
+my_age = 27
+
+print(my_age)
+
+
+'''lo que vamos a ver a continuacion es utiliar imput en la programación ,
+imput es para darle una instruccion a la computadora para decir que nosotros ingresemos ese valor
+por ejemplo '''
+
+my_name = input("¿cual es tu nombre?")
+
+"""imput es una funcion es python que recoje un valor desde la terminal y python se queda en la espera 
+para que nosotros le dijitemos su nombre  """
+print("usando input " , my_name )
+
+
+
+
+
