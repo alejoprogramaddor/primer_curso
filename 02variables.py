d97e57e805108af567c81b66937c8fe4d04dc8f7
alejoprogramaddor <aristizabal-1996@hotmail.com>
2024-02-14 18:47:41 -0500

    segundo estudio de python basico

---
+++ 02variables.py
@@ -0,0 +1,32 @@
+'''TIPOS DE DATOS 
+STRING = este es una cadena de texto o una cadena de carateres y se 
+reconoce por que se inicia con comilla doble por ejemplo '''
+
+my_name = "alejandro"
+my_name = "sebastian"
+print(my_name)
+
+"""para observar que tipo de dato es podemos utilizar el 
+TYPE , este se utiliza para poder saber que tipo de datos es , 
+por ejemplo """
+
+print(type(my_name))
+
+'''en la parte de la terminal podemos observar que aparaece class 'str' informando que 
+lo que se vio acontinuacion es un string '''
+
+"""los valores boleanos tambien se utiliza para poder decir falso o verdadero 
+para poder verdadero o falso tiene que ir con la primera en mayuscula 
+por ejemplo True o False """
+
+is_single = True 
+print('is_single =>', is_single)
+print(type(is_single))
+
+#imputs 
+my_age = input('¿cual es tu edad ? ')
+print('my_age ',my_age)
+print(type(my_age))
+
+
+
