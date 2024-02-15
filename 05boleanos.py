00162fc10a04aa99e871628e7d9945a7e42615aa
alejoprogramaddor <aristizabal-1996@hotmail.com>
2024-02-14 19:11:34 -0500

    boleanos

---
+++ 05boleanos.py
@@ -0,0 +1,15 @@
+is_single = True 
+print(type(is_single))
+'''en la parte de la terminal va a aparecer que es tipo '''
+is_single = False 
+print(type(is_single))
+
+'''para intercambiar el valor , se utiliza NOT y se intercambia el valor boleano por ejemplo
+not lo que es , es un operador '''
+print(not True)
+print(not False)
+
+'''tambien se puede transformar de la siguiente manera '''
+is_single = not is_single
+print(is_single)
+
