1f382cf62d41f178f95bdcf88bbbe9c785306364
alejoprogramaddor <aristizabal-1996@hotmail.com>
2024-02-15 10:53:35 -0500

    listas

---
+++ 16 listas.py
@@ -0,0 +1,35 @@
+numbers = [1, 2, 3, 4, 5, 6]
+
+print(numbers)
+
+print(type(numbers))
+
+task = ['lavar los platos' , 'jugar pley']
+
+print(task)
+
+# se puede hacer una lista de numeros naturales , string , boleanos por ejemplo 
+
+type = [1, True , 'hola'] 
+
+print(type)
+
+# en esta parte podemos poner que parte de la lista esta , por ejemplo 0 es el primer caracter 
+
+print(task[0])
+
+text = 'hola'
+
+# text[0] = 'w'
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+task[0] = 'sacar la perra'
+
+print(task)
+
+# como podemos observar en la parte de la taks que son tareas ,
+# lo que hacemos es cambiar la primera oracion que es lavar los platos por a diferencia de sacar la perra 
+
+#----------------------------------------------------------------------------------------------------------------------------
+
+# se puiede cambiar la misma oracion las veces que nosotros queramos 
+
