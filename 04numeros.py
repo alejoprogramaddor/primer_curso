a2e93de0bde11a1bfd96413e581705b775440190
alejoprogramaddor <aristizabal-1996@hotmail.com>
2024-02-14 19:10:23 -0500

    numeros

---
+++ 04numeros.py
@@ -0,0 +1,43 @@
+"""por ejemplo en un juego hay 3 vidas """
+lives = 3
+print(type(lives))
+
+'''nos va a aparecer en terminal que es int = que significa que es un numero entero, a la 
+hora de declarar un numero no lo puede declarar entre comillas , por que se va a declarar como 
+un string en la variable '''
+
+#por ejemplo la edad de una persona 
+age = 27
+#el presupuesto que tiene una persona que en ingles significa badget 
+budget = 100 
+#por ejemplo la temperatura con decimal 
+temperature = 25.33
+print(type(temperature))
+"""en la parte de la terminal , ya nos va a informar que es un flotante que va a aparecer como un 'FLOAT'"""
+
+lives = 2 
+print(lives)
+lives = 1 
+print(lives)
+
+lives = 12 + 15 
+print(lives)
+lives = lives - 1 
+print(lives)
+
+lives -= 1 
+print(lives)
+
+lives += 1 
+print(lives)
+
+number = 450000000000000000000.15
+print(number)
+
+number_b = 0.00000000002
+print(number_b)
+''' lo que pasa con numeros muy grandes es que en la terminal va a aparecer la anotacion cientifica, 
+no le va aponer todas las notaciones de 0-0000000002 si no que en vez de eso se pone un 2e-11'''
+
+
+
