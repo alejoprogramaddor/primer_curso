538e2e5760ed52cf5bae99fed6da1855311437af
alejoprogramaddor <aristizabal-1996@hotmail.com>
2024-02-15 10:41:02 -0500

    operadores de comparacion

---
+++ 08 operadores_comparacion.py
@@ -0,0 +1,29 @@
+'''estos sirven para que hagan comparadores de logica, 
+acontinuacion el siquiente ejemplo '''
+
+#  >    este es mayor que 
+
+print(10>9)
+
+#  <    este es menor que 
+
+print(11<10)
+
+#  >=   este es mayor o igual que 
+
+print(11>=11)
+#  <=   este es menor o igual que 
+
+print(68<=89)
+#  ==   este es estictamente igualdad 
+
+print(6 == 6)
+
+#  !=   este es diferente de 
+
+print(6 != 6 )
+print("aple" == 'aple') #esta va a dar True
+print("aple" == "Aple") #esta va a dar False , por la mayuscula
+
+print('aple' != 'apple')
+
