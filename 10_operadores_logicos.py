dbcb572fe93ac5bb0c6715ac754c4e2d1c30adf0
alejoprogramaddor <aristizabal-1996@hotmail.com>
2024-02-15 10:44:57 -0500

    operadores logicos

---
+++ 10_operadores_logicos.py
@@ -0,0 +1,39 @@
+# operador de and 
+# esta es la tabla de el operador AND 
+
+print('AND')
+print('True and True = ', True and True)
+print('True and false = ', True and False)
+print('False and True =', False and True)
+print('False and False =', False and False)
+
+#solamente si es true y true , va a dar true , del resto siempre va a dar false , de es se trata and 
+
+print(10 > 5 and 5 < 10  )
+
+#operador or 
+#esta en la tabla del operador OR
+
+print('OR')
+print('True or True = ', True or True)
+print('True or false = ', True or False)
+print('False or True =', False or True)
+print('False 0r False =', False or False)
+
+#siempre y cuando este fucnionando una sea verdadera , siempre va a ser TRUE 
+#la unica forma de que no de true es falso y falso .  
+
+#un ejemplo practico de utilizar un or utilizando el rol de un usuario 
+
+role = input('Digita el rol')
+
+print(role == 'admin' or role == 'vendedor')
+
+'''en esta parte va a aparecer un imput y le va a pedir que escriba , si le da admin o vendedor va a 
+aparecer true , pero ni aparece otro nombre , va a aparecer error '''
+
+
+
+
+
+
