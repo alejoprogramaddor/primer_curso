f8a3b4962e954ad757c79c188285a90286215abb
alejoprogramaddor <aristizabal-1996@hotmail.com>
2024-02-15 10:29:34 -0500

    operaciones aritmeticas

---
+++ 07operadores_aritmeticos.py
@@ -0,0 +1,53 @@
+print(10 + 10 )
+print(10 - 10 )
+print(10 * 2 )
+print(10 / 2 )
+
+print('hola' + ' mundo')
+print('hola ' * 3)
+
+'''haca lo que vemos es que se va a multiplicar ese string 3 veces '''
+
+print(10 % 3)
+
+'''lo que va aparecer en la consola es un 1 ya que es el 
+residuo de la division '''
+
+print(100 // 3)
+#en esta parte si nos va aparecer el numero entero de esa division 
+
+print(2 ** 3)
+#esta se utiliza para que se eleve al numero del final , en este 
+# caso es a la 3
+
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+'''hay que saber el orden en el que se cuadran las operaciones aritmeticas 
+para saber por que dio 10 en el ejemplo pasado '''
+
+#en python se puede hacer lo siguiente 
+
+print(2 + 3 * 4)
+
+'''en este caso se va a multiplicar primero y despues sumar '''
+
+print((2 + 3) * 4)
+
+'''en este caso se va a sumar primero y despues multiplicar '''
+
+'''
+en python se van a hacer las primeras operaciones matematicas 
+
+1ro : parentesis
+2do : exponentes
+3ro : multiplicacion(*)
+4to: division (/)
+5to: adicion (suma)
+6to: sustracion (resta)
+
+y sempre va a ir la prioridad de izquierda a derecha 
+'''
+
+
+print(10//3)
+
